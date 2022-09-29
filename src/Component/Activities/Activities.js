f1dff24b40eb1b127d152ead8ea8e2601444224d
Subirdas29 <dasjoy104@gmail.com>
2022-09-29 17:27:05 +0600

    handle added

--- src/Component/Activities/Activities.js
+++ src/Component/Activities/Activities.js
@@ -5,22 +5,35 @@ import './Activities.css'
 
 const Activities = () => {
     const [activities, setActivities]= useState([]);
+    const[time,setTime] = useState([])
     
     useEffect(()=>{
         fetch('activity.json')
         .then(res=>res.json())
         .then(data=> setActivities(data))
     },[]);
-
-
+    
+    const handleAddToTime = (activity) =>
+    {
+        const newTime = [...time, activity]
+        setTime(newTime);
+    }
     return (
         <div>
             <span>Selected the exercise</span>
         <div className='activities-details'>
             <div className='activity-info'>
                 {
-                activities.map(activity=> <Activity activity={activity}></Activity> )
+                activities.map(activity=> 
+                <Activity
+                 key={activity.id}
+                    activity={activity}
+                    handleAddToTime={handleAddToTime}>
+
+                    </Activity>)
+                    
                 }
+                
             
             </div>
             <div className='exercise-details'>
